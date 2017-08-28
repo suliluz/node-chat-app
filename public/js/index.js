22b385a20d8e1f500553fe36d27bb00bdee3126c
Suliluz Marcus Marrio <marriomarcuslogin@gmail.com>
2017-08-29 01:06:40 +0800

    Form added and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -12,13 +12,21 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (newMessage) {
-  console.log('Got New Message', newMessage);
+socket.on('newMessage', function (message) {
+  console.log('Got New Message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it!', data);
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
